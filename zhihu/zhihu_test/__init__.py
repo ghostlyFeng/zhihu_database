bbb24a9271a76337ee344c95fe8ae83449e9a617
ghostlyfeng <ghostlyfeng@outlook.com>
2018-05-09 20:14:12 +0800

    zhihu_database

---
+++ zhihu/zhihu_test/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2018/5/4 14:28
+# @Author  : Hf
+
---
+++ zhihu/zhihu_test/zhihu_config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+mysql_config = {
+    'host': '127.0.0.1',
+    'port': 3306,
+    'user': 'root',
+    'passwd': 'qwe123',
+    'db': 'zhihu_daily_report',
+    'charset': 'utf8'
+}
+
+spe_config = mysql_config
+
